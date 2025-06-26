3a05cee2d34bf833e93699d13b5e48de2ac54f30
Luna Nightingale <nicholas.jeffrey50@gmail.com>
2025-06-26 21:43:43 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,303 @@
+XP = SpriteKind.create()
+
+char_level = 1
+xp_bar = statusbars.create(120, 4, StatusBarKind.magic)
+xp_bar.bottom = 110
+xp_bar.max = 10
+xp_bar.value = 0
+lta: number = 0
+xp_to_add = 0
+xp_bar.set_flag(SpriteFlag.INVISIBLE, True)
+gstate = "battle"
+
+text_sprite = textsprite.create(str(xp_to_add))
+text_sprite.set_icon(assets.image("EXP"))
+text_sprite.left = 0
+text_sprite.top = 0
+text_sprite.set_outline(1, 6)
+
+def rotate(xp: Sprite):
+    if randint(0,1) == 1:
+        for i in range(0, randint(0, 120)):
+            transformSprites.change_rotation(xp, 3)
+    else:
+        for i in range(0, randint(0, 120)):
+            transformSprites.change_rotation(xp, -3)
+
+
+def go_to_xp():
+        global lta
+        for pip in sprites.all_of_kind(XP):
+            global lta
+            transformSprites.rotate_sprite(pip, -90)
+            story.sprite_move_to_location(pip, xp_bar.x, xp_bar.y, 500)
+            sprites.destroy(pip, effects.rings, 50)
+            xp_bar.value +=1
+            if xp_bar.value == xp_bar.max:
+                lta +=1
+                console.log(lta)
+                xp_bar.value = 0
+                xp_bar.max += 20
+                effects.bubbles.start_screen_effect(500)
+
+def level():
+    global xp_to_add, gstate, h1a, h1h, h1atk, h1def, lta
+    n = 0
+    xp_bar.set_flag(SpriteFlag.INVISIBLE, False)
+    while n < xp_to_add:
+
+
+        xp = sprites.create(assets.image("EXP"), XP)
+        xp.scale = 0.5
+        story.sprite_move_to_location(xp, randint(60,90), randint(40,70), 300)
+        def startrotate():
+            rotate(xp)
+        timer.background(startrotate)
+        n += 1
+        text_sprite.set_text(str(xp_to_add - n))
+
+    go_to_xp()
+    xp_to_add = 0
+    console.log(lta)
+    for i in range (0,lta):
+        h1a -= 10
+        h1h += 25
+        h1atk += 10
+        h1def += 5
+    sprites.set_data_number(hero, "agi", h1a)
+    sprites.set_data_number(hero, "atk", h1atk)
+    sprites.set_data_number(hero, "def", h1def)
+    sprites.set_data_number(hero, "hp", h1h)
+    console.log("agi" + sprites.read_data_number(hero, "agi") + " ATK: " + sprites.read_data_number(hero, "atk") + " DEF: " + sprites.read_data_number(hero, "def") + " HP: " + sprites.read_data_number(hero, "hp"))
+    for bar in statusbars.all_of_kind(1):
+        sprites.destroy(bar)
+    for bars in statusbars.all_of_kind(2):
+            sprites.destroy(bars)
+    sprites.set_data_number(boss, "atk", batk)
+    sprites.set_data_number(boss, "def", bdef)
+    sprites.set_data_number(boss, "agi", ba)
+    setup_bars(hero)
+    setup_bars(boss)
+    lta = 0
+    gstate = "battle"
+    xp_bar.set_flag(SpriteFlag.INVISIBLE, True)
+#level()
+
+
+
+Actor = SpriteKind.create()
+Party = SpriteKind.create()
+hero = sprites.create(assets.image("h1"), Actor)
+hero.set_position(101, 66)
+
+boss = sprites.create(assets.image("e1"), Actor)
+boss.set_position(29, 76)
+scene.set_background_image(assets.image("bg"))
+
+
+
+bmenuopen = False
+my_menu: miniMenu.MenuSprite = None
+h1a = 100
+ba = 170
+bh = 100
+h1h = 125
+batk = 25
+h1atk = 50
+bdef = 10
+h1def = 10
+
+sprites.set_data_boolean(hero, "acting", False)
+sprites.set_data_boolean(boss, "acting", False)
+sprites.set_data_number(hero, "agi", h1a)
+sprites.set_data_number(boss, "agi", ba)
+sprites.set_data_string(hero, "nam", "Hero")
+sprites.set_data_number(hero, "hp", h1h)
+sprites.set_data_number(boss, "hp", bh)
+sprites.set_data_number(hero, "atk", h1atk)
+sprites.set_data_number(boss, "atk", batk)
+sprites.set_data_number(hero, "def", h1def)
+sprites.set_data_number(boss, "def", bdef)
+
+
+
+def setup_bars(char: Sprite):
+    agi_bar = statusbars.create(20, 4, 2)
+    agi_bar.attach_to_sprite(char,2)
+    agi_bar.max = sprites.read_data_number(char, "agi")
+    agi_bar.value = 0
+    health_bar = statusbars.create(20,4,1)
+    health_bar.attach_to_sprite(char, 4)
+    health_bar.max = sprites.read_data_number(char, "hp")
+    health_bar.value = health_bar.max
+
+
+for char in sprites.all_of_kind(Actor):
+    setup_bars(char)
+hero.set_kind(Party)
+
+
+
+
+def bar_prog(char: Sprite):
+    global bmenuopen
+    if sprites.read_data_boolean(char, "acting") == True:
+        return
+    if statusbars.get_status_bar_attached_to(2, char).value == statusbars.get_status_bar_attached_to(2, char).max-1 and bmenuopen and char.kind() == Party:
+            return
+    else:
+        statusbars.get_status_bar_attached_to(2, char).value += 1
+    if statusbars.get_status_bar_attached_to(2, char).value == statusbars.get_status_bar_attached_to(2, char).max and bmenuopen and char.kind() == Party:
+        return
+    if statusbars.get_status_bar_attached_to(2, char).value == statusbars.get_status_bar_attached_to(2, char).max and char.kind() == Party:
+        bmenuopen = True
+        pmenu(char)
+
+    elif statusbars.get_status_bar_attached_to(2, char).value == statusbars.get_status_bar_attached_to(2, char).max and char.kind() == Actor:
+        statusbars.get_status_bar_attached_to(2, char).value = 0
+        sprites.set_data_boolean(char, "acting", True)
+        def resenatt():
+            enemy_att(char)
+        timer.background(resenatt)
+
+
+def pmenu(char: Sprite):
+    global bmenuopen
+    my_menu = miniMenu.create_menu(miniMenu.create_menu_item("Attack"),miniMenu.create_menu_item("Defend"), miniMenu.create_menu_item("Special"))
+    my_menu.set_title(sprites.read_data_string(char, "nam"))
+
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.ROWS, 2)
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.COLUMNS, 2)
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 120)
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 40)
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.BACKGROUND_COLOR, 1)
+    my_menu.y = 100
+    my_menu.x = 80
+    my_menu.set_menu_style_property(miniMenu.MenuStyleProperty.USE_AS_TEMPLATE, 1)
+    def on_button_pressed(selection, selectedIndex):
+        global bmenuopen
+
+        statusbars.get_status_bar_attached_to(2, char).value = 0
+        my_menu.close()
+        if selectedIndex == 0:
+            bmenuopen = False
+            sprites.set_data_boolean(char, "acting", True)
+            def resolveatt():
+                p_attack(char)
+            timer.background(resolveatt)
+        elif selectedIndex == 1:
+            bmenuopen = False
+            return
+        elif selectedIndex == 2:
+            sprites.set_data_boolean(char, "acting", True)
+            if sprites.read_data_number(char, "agi") == h1a:
+                
+                def resh1sp():
+                    h1_specials(char)
+                timer.background(resh1sp)
+    my_menu.on_button_pressed(controller.A, on_button_pressed)
+
+def p_attack(char: Sprite):
+
+    story.sprite_move_to_location(char, char.x-20, char.y, 100)
+    pause(500)
+    story.sprite_move_to_location(char, char.x+20, char.y, 100)
+    sprites.set_data_boolean(char, "acting", False)
+    fxspri = sprites.create(assets.image("blank"))
+    fxspri.set_position(boss.x, boss.y)
+    fxspri.z = 100
+    animation.run_image_animation(fxspri,assets.animation("attsp"), 50, False)
+    pause(150)
+    statusbars.get_status_bar_attached_to(1, boss).value -= sprites.read_data_number(char, "atk") - sprites.read_data_number(boss, "def") +1
+    fxspri.destroy()
+
+def h1_specials(char: Sprite):
+    global bmenuopen
+    my_menu = miniMenu.create_menu(miniMenu.create_menu_item("Fire"),miniMenu.create_menu_item("Big Hit"),)
+    my_menu.set_title(sprites.read_data_string(char, "nam") + " Specials")
+    my_menu.y = 100
+    my_menu.x = 80
+    def on_button_pressed(selection, selectedIndex):
+        global bmenuopen
+        my_menu.close()
+        bmenuopen = False
+        if selectedIndex == 0:
+            def ressp1():
+                h1_fire(char)
+            timer.background(ressp1)
+        else:
+            def ressp2():
+                h1_bh(char)
+            timer.background(ressp2)
+    my_menu.on_button_pressed(controller.A, on_button_pressed)
+
+
+def h1_fire(char: Sprite):
+    story.sprite_move_to_location(char, char.x-20, char.y, 100)
+    pause(500)
+    story.sprite_move_to_location(char, char.x+20, char.y, 100)
+    sprites.set_data_boolean(char, "acting", False)
+    fxspri = sprites.create(assets.image("blank"))
+    fxspri.set_position(boss.x, boss.y)
+    fxspri.z = 100
+    animation.run_image_animation(fxspri,assets.animation("fire"), 50, False)
+    pause(200)
+    statusbars.get_status_bar_attached_to(1, boss).value -= sprites.read_data_number(char, "atk") - sprites.read_data_number(boss, "def") + 20
+    fxspri.destroy()
+
+def h1_bh(char: Sprite):
+    story.sprite_move_to_location(char, char.x-20, char.y, 100)
+    pause(500)
+    story.sprite_move_to_location(char, char.x+20, char.y, 100)
+    sprites.set_data_boolean(char, "acting", False)
+    fxspri = sprites.create(assets.image("blank"))
+    fxspri.set_position(boss.x, boss.y)
+    fxspri.z = 100
+    animation.run_image_animation(fxspri,assets.animation("big"), 50, False)
+    pause(250)
+    statusbars.get_status_bar_attached_to(1, boss).value -= sprites.read_data_number(char, "atk") - sprites.read_data_number(boss, "def") + 10
+    fxspri.destroy()
+
+
+def enemy_att(char: Sprite):
+    story.sprite_move_to_location(char, char.x+20, char.y, 100)
+    pause(500)
+    story.sprite_move_to_location(char, char.x-20, char.y, 100)
+    sprites.set_data_boolean(char, "acting", False)
+    fxspri = sprites.create(assets.image("blank"))
+    target = sprites.all_of_kind(Party)[0]
+    fxspri.set_position(target.x, target.y)
+    fxspri.z = 100
+    animation.run_image_animation(fxspri,assets.animation("attsp"), 50, False)
+    pause(150)
+    statusbars.get_status_bar_attached_to(1, hero).value -= sprites.read_data_number(char, "atk") - sprites.read_data_number(hero, "def")
+    fxspri.destroy()
+
+def tick():
+    global gstate
+    if gstate == "battle":
+        for member in sprites.all_of_kind(Party):
+            bar_prog(member)
+        bar_prog(boss)
+game.on_update(tick)
+
+def on_zero(status):
+    global gstate, xp_to_add
+    if statusbars.get_status_bar_attached_to(1, boss).value <= 0:
+        boss.start_effect(effects.cool_radial, 500)
+        statusbars.get_status_bar_attached_to(1, boss).value = statusbars.get_status_bar_attached_to(1, boss).max
+        if sprites.read_data_number(boss, "agi") > 5:
+            sprites.set_data_number(boss, "agi", sprites.read_data_number(boss, "agi")-15)
+        sprites.set_data_number(boss, "atk", sprites.read_data_number(boss, "atk")+5)
+        sprites.set_data_number(boss, "def", sprites.read_data_number(boss, "def")+5)
+        sprites.set_data_number(boss, "hp", sprites.read_data_number(boss, "hp")+25)
+        xp_to_add += randint(8,12)
+        console.log("def: " + sprites.read_data_number(boss, "def") + " atk: " + sprites.read_data_number(boss, "atk") + " agi: " + sprites.read_data_number(boss, "agi"))
+        text_sprite.set_text(str(xp_to_add))
+        sprites.destroy(statusbars.get_status_bar_attached_to(2, boss))
+        sprites.destroy(statusbars.get_status_bar_attached_to(1, boss))
+        setup_bars(boss)
+    elif statusbars.get_status_bar_attached_to(1, hero).value <= 0:
+        gstate = "level"
+        level()
+statusbars.on_zero(StatusBarKind.health, on_zero)
---
+++ main.ts
@@ -0,0 +1,318 @@
+let XP = SpriteKind.create()
+let char_level = 1
+let xp_bar = statusbars.create(120, 4, StatusBarKind.Magic)
+xp_bar.bottom = 110
+xp_bar.max = 10
+xp_bar.value = 0
+let lta = 0
+let xp_to_add = 0
+xp_bar.setFlag(SpriteFlag.Invisible, true)
+let gstate = "battle"
+let text_sprite = textsprite.create("" + xp_to_add)
+text_sprite.setIcon(assets.image`EXP`)
+text_sprite.left = 0
+text_sprite.top = 0
+text_sprite.setOutline(1, 6)
+function rotate(xp: Sprite) {
+    let i: number;
+    if (randint(0, 1) == 1) {
+        for (i = 0; i < randint(0, 120); i++) {
+            transformSprites.changeRotation(xp, 3)
+        }
+    } else {
+        for (i = 0; i < randint(0, 120); i++) {
+            transformSprites.changeRotation(xp, -3)
+        }
+    }
+    
+}
+
+function go_to_xp() {
+    
+    for (let pip of sprites.allOfKind(XP)) {
+        
+        transformSprites.rotateSprite(pip, -90)
+        story.spriteMoveToLocation(pip, xp_bar.x, xp_bar.y, 500)
+        sprites.destroy(pip, effects.rings, 50)
+        xp_bar.value += 1
+        if (xp_bar.value == xp_bar.max) {
+            lta += 1
+            console.log(lta)
+            xp_bar.value = 0
+            xp_bar.max += 20
+            effects.bubbles.startScreenEffect(500)
+        }
+        
+    }
+}
+
+function level() {
+    let xp: Sprite;
+    
+    let n = 0
+    xp_bar.setFlag(SpriteFlag.Invisible, false)
+    while (n < xp_to_add) {
+        xp = sprites.create(assets.image`EXP`, XP)
+        xp.scale = 0.5
+        story.spriteMoveToLocation(xp, randint(60, 90), randint(40, 70), 300)
+        timer.background(function startrotate() {
+            rotate(xp)
+        })
+        n += 1
+        text_sprite.setText("" + (xp_to_add - n))
+    }
+    go_to_xp()
+    xp_to_add = 0
+    console.log(lta)
+    for (let i = 0; i < lta; i++) {
+        h1a -= 10
+        h1h += 25
+        h1atk += 10
+        h1def += 5
+    }
+    sprites.setDataNumber(hero, "agi", h1a)
+    sprites.setDataNumber(hero, "atk", h1atk)
+    sprites.setDataNumber(hero, "def", h1def)
+    sprites.setDataNumber(hero, "hp", h1h)
+    console.log("agi" + sprites.readDataNumber(hero, "agi") + " ATK: " + sprites.readDataNumber(hero, "atk") + " DEF: " + sprites.readDataNumber(hero, "def") + " HP: " + sprites.readDataNumber(hero, "hp"))
+    for (let bar of statusbars.allOfKind(1)) {
+        sprites.destroy(bar)
+    }
+    for (let bars of statusbars.allOfKind(2)) {
+        sprites.destroy(bars)
+    }
+    sprites.setDataNumber(boss, "atk", batk)
+    sprites.setDataNumber(boss, "def", bdef)
+    sprites.setDataNumber(boss, "agi", ba)
+    setup_bars(hero)
+    setup_bars(boss)
+    lta = 0
+    gstate = "battle"
+    xp_bar.setFlag(SpriteFlag.Invisible, true)
+}
+
+// level()
+let Actor = SpriteKind.create()
+let Party = SpriteKind.create()
+let hero = sprites.create(assets.image`h1`, Actor)
+hero.setPosition(101, 66)
+let boss = sprites.create(assets.image`e1`, Actor)
+boss.setPosition(29, 76)
+scene.setBackgroundImage(assets.image`bg`)
+let bmenuopen = false
+let my_menu : miniMenu.MenuSprite = null
+let h1a = 100
+let ba = 170
+let bh = 100
+let h1h = 125
+let batk = 25
+let h1atk = 50
+let bdef = 10
+let h1def = 10
+sprites.setDataBoolean(hero, "acting", false)
+sprites.setDataBoolean(boss, "acting", false)
+sprites.setDataNumber(hero, "agi", h1a)
+sprites.setDataNumber(boss, "agi", ba)
+sprites.setDataString(hero, "nam", "Hero")
+sprites.setDataNumber(hero, "hp", h1h)
+sprites.setDataNumber(boss, "hp", bh)
+sprites.setDataNumber(hero, "atk", h1atk)
+sprites.setDataNumber(boss, "atk", batk)
+sprites.setDataNumber(hero, "def", h1def)
+sprites.setDataNumber(boss, "def", bdef)
+function setup_bars(char: Sprite) {
+    let agi_bar = statusbars.create(20, 4, 2)
+    agi_bar.attachToSprite(char, 2)
+    agi_bar.max = sprites.readDataNumber(char, "agi")
+    agi_bar.value = 0
+    let health_bar = statusbars.create(20, 4, 1)
+    health_bar.attachToSprite(char, 4)
+    health_bar.max = sprites.readDataNumber(char, "hp")
+    health_bar.value = health_bar.max
+}
+
+for (let char of sprites.allOfKind(Actor)) {
+    setup_bars(char)
+}
+hero.setKind(Party)
+function bar_prog(char: Sprite) {
+    
+    if (sprites.readDataBoolean(char, "acting") == true) {
+        return
+    }
+    
+    if (statusbars.getStatusBarAttachedTo(2, char).value == statusbars.getStatusBarAttachedTo(2, char).max - 1 && bmenuopen && char.kind() == Party) {
+        return
+    } else {
+        statusbars.getStatusBarAttachedTo(2, char).value += 1
+    }
+    
+    if (statusbars.getStatusBarAttachedTo(2, char).value == statusbars.getStatusBarAttachedTo(2, char).max && bmenuopen && char.kind() == Party) {
+        return
+    }
+    
+    if (statusbars.getStatusBarAttachedTo(2, char).value == statusbars.getStatusBarAttachedTo(2, char).max && char.kind() == Party) {
+        bmenuopen = true
+        pmenu(char)
+    } else if (statusbars.getStatusBarAttachedTo(2, char).value == statusbars.getStatusBarAttachedTo(2, char).max && char.kind() == Actor) {
+        statusbars.getStatusBarAttachedTo(2, char).value = 0
+        sprites.setDataBoolean(char, "acting", true)
+        timer.background(function resenatt() {
+            enemy_att(char)
+        })
+    }
+    
+}
+
+function pmenu(char: Sprite) {
+    
+    let my_menu = miniMenu.createMenu(miniMenu.createMenuItem("Attack"), miniMenu.createMenuItem("Defend"), miniMenu.createMenuItem("Special"))
+    my_menu.setTitle(sprites.readDataString(char, "nam"))
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Rows, 2)
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Columns, 2)
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Width, 120)
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Height, 40)
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.BackgroundColor, 1)
+    my_menu.y = 100
+    my_menu.x = 80
+    my_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.UseAsTemplate, 1)
+    my_menu.onButtonPressed(controller.A, function on_button_pressed(selection: any, selectedIndex: any) {
+        
+        statusbars.getStatusBarAttachedTo(2, char).value = 0
+        my_menu.close()
+        if (selectedIndex == 0) {
+            bmenuopen = false
+            sprites.setDataBoolean(char, "acting", true)
+            timer.background(function resolveatt() {
+                p_attack(char)
+            })
+        } else if (selectedIndex == 1) {
+            bmenuopen = false
+            return
+        } else if (selectedIndex == 2) {
+            sprites.setDataBoolean(char, "acting", true)
+            if (sprites.readDataNumber(char, "agi") == h1a) {
+                timer.background(function resh1sp() {
+                    h1_specials(char)
+                })
+            }
+            
+        }
+        
+    })
+}
+
+function p_attack(char: Sprite) {
+    story.spriteMoveToLocation(char, char.x - 20, char.y, 100)
+    pause(500)
+    story.spriteMoveToLocation(char, char.x + 20, char.y, 100)
+    sprites.setDataBoolean(char, "acting", false)
+    let fxspri = sprites.create(assets.image`blank`)
+    fxspri.setPosition(boss.x, boss.y)
+    fxspri.z = 100
+    animation.runImageAnimation(fxspri, assets.animation`attsp`, 50, false)
+    pause(150)
+    statusbars.getStatusBarAttachedTo(1, boss).value -= sprites.readDataNumber(char, "atk") - sprites.readDataNumber(boss, "def") + 1
+    fxspri.destroy()
+}
+
+function h1_specials(char: Sprite) {
+    
+    let my_menu = miniMenu.createMenu(miniMenu.createMenuItem("Fire"), miniMenu.createMenuItem("Big Hit"))
+    my_menu.setTitle(sprites.readDataString(char, "nam") + " Specials")
+    my_menu.y = 100
+    my_menu.x = 80
+    my_menu.onButtonPressed(controller.A, function on_button_pressed(selection: any, selectedIndex: any) {
+        
+        my_menu.close()
+        bmenuopen = false
+        if (selectedIndex == 0) {
+            timer.background(function ressp1() {
+                h1_fire(char)
+            })
+        } else {
+            timer.background(function ressp2() {
+                h1_bh(char)
+            })
+        }
+        
+    })
+}
+
+function h1_fire(char: Sprite) {
+    story.spriteMoveToLocation(char, char.x - 20, char.y, 100)
+    pause(500)
+    story.spriteMoveToLocation(char, char.x + 20, char.y, 100)
+    sprites.setDataBoolean(char, "acting", false)
+    let fxspri = sprites.create(assets.image`blank`)
+    fxspri.setPosition(boss.x, boss.y)
+    fxspri.z = 100
+    animation.runImageAnimation(fxspri, assets.animation`fire`, 50, false)
+    pause(200)
+    statusbars.getStatusBarAttachedTo(1, boss).value -= sprites.readDataNumber(char, "atk") - sprites.readDataNumber(boss, "def") + 20
+    fxspri.destroy()
+}
+
+function h1_bh(char: Sprite) {
+    story.spriteMoveToLocation(char, char.x - 20, char.y, 100)
+    pause(500)
+    story.spriteMoveToLocation(char, char.x + 20, char.y, 100)
+    sprites.setDataBoolean(char, "acting", false)
+    let fxspri = sprites.create(assets.image`blank`)
+    fxspri.setPosition(boss.x, boss.y)
+    fxspri.z = 100
+    animation.runImageAnimation(fxspri, assets.animation`big`, 50, false)
+    pause(250)
+    statusbars.getStatusBarAttachedTo(1, boss).value -= sprites.readDataNumber(char, "atk") - sprites.readDataNumber(boss, "def") + 10
+    fxspri.destroy()
+}
+
+function enemy_att(char: Sprite) {
+    story.spriteMoveToLocation(char, char.x + 20, char.y, 100)
+    pause(500)
+    story.spriteMoveToLocation(char, char.x - 20, char.y, 100)
+    sprites.setDataBoolean(char, "acting", false)
+    let fxspri = sprites.create(assets.image`blank`)
+    let target = sprites.allOfKind(Party)[0]
+    fxspri.setPosition(target.x, target.y)
+    fxspri.z = 100
+    animation.runImageAnimation(fxspri, assets.animation`attsp`, 50, false)
+    pause(150)
+    statusbars.getStatusBarAttachedTo(1, hero).value -= sprites.readDataNumber(char, "atk") - sprites.readDataNumber(hero, "def")
+    fxspri.destroy()
+}
+
+game.onUpdate(function tick() {
+    
+    if (gstate == "battle") {
+        for (let member of sprites.allOfKind(Party)) {
+            bar_prog(member)
+        }
+        bar_prog(boss)
+    }
+    
+})
+statusbars.onZero(StatusBarKind.Health, function on_zero(status: StatusBarSprite) {
+    
+    if (statusbars.getStatusBarAttachedTo(1, boss).value <= 0) {
+        boss.startEffect(effects.coolRadial, 500)
+        statusbars.getStatusBarAttachedTo(1, boss).value = statusbars.getStatusBarAttachedTo(1, boss).max
+        if (sprites.readDataNumber(boss, "agi") > 5) {
+            sprites.setDataNumber(boss, "agi", sprites.readDataNumber(boss, "agi") - 15)
+        }
+        
+        sprites.setDataNumber(boss, "atk", sprites.readDataNumber(boss, "atk") + 5)
+        sprites.setDataNumber(boss, "def", sprites.readDataNumber(boss, "def") + 5)
+        sprites.setDataNumber(boss, "hp", sprites.readDataNumber(boss, "hp") + 25)
+        xp_to_add += randint(8, 12)
+        console.log("def: " + sprites.readDataNumber(boss, "def") + " atk: " + sprites.readDataNumber(boss, "atk") + " agi: " + sprites.readDataNumber(boss, "agi"))
+        text_sprite.setText("" + xp_to_add)
+        sprites.destroy(statusbars.getStatusBarAttachedTo(2, boss))
+        sprites.destroy(statusbars.getStatusBarAttachedTo(1, boss))
+        setup_bars(boss)
+    } else if (statusbars.getStatusBarAttachedTo(1, hero).value <= 0) {
+        gstate = "level"
+        level()
+    }
+    
+})
